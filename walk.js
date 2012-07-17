ce7b1bc1bee9db9d467221411315fe2216493f2a
Bill Ramsey <bill@billiamram.com>
2012-07-17 18:05:46 -0400

    wrapped and commented code

--- walk.js
+++ walk.js
@@ -1,12 +1,15 @@
 (function () {
     
     var fs = require('fs');
+
+    // TODO: write each method if that's all I use from underscore.
     var _ = require('underscore');
     var parse = require('esprima').parse;
 
     var file = fs.readFileSync('example.js', 'ascii');
 
 
+    // build the ast with esprima.
     var tree = parse(file, {range: true});
 
     var result = {
@@ -15,17 +18,20 @@
         inspect : function () { return result.toString() }
     };
 
+    // Used to insert helpers during  the ast walk.
     var insertHelpers = function(node, parent) {
         if (!node.range || node.parent) return;
 
-        node.parent = parent;
-
+        node.parent = parent; // reference to parent node.
+        
+        // returns the current source for the node.
         node.source = function () {
             return result.chunks.slice(
                 node.range[0], node.range[1] + 1
             ).join('');
         };
 
+        // updates the source for the node.
         node.update = function (s) {
             result.chunks[node.range[0]] = s;
             for (var i = node.range[0] + 1; i < node.range[1] + 1; i++) {
@@ -55,16 +61,22 @@
         return;
     };
 
+    /**
+     * Generic walk function
+     * @action: function to be applied to the node in order.
+     */
     var walk = function(node, parent, action) {
         insertHelpers(node, parent);
         if (action) action(node);
+
         for (var key in node) {
             if (key === 'parent') return;
         
             var child = node[key];
             if (child instanceof Array) {
                 for (var i=0, l=child.length; i<l; i++) {
-                    if (child[i] && typeof child[i] === 'object' && child[i].type) {
+                    if (child[i] && typeof child[i] === 'object' && 
+                            child[i].type) {
                         walk(child[i], node, action);
                     }
                 }
@@ -75,15 +87,7 @@
         }
     };
 
-    var deleteWalk = function(node) {
-
-        if(node.type && 
-            (node.type === 'FunctionExpression' || node.type === 'FunctionDeclaration') &&
-            !node.visited) {
-                removeDeclaration(node);
-        }
-    };
-    
+    // Checks if the node type affects scope.
     var affectsScope = function(type) {
         switch(type) {
             case 'Program':
@@ -121,7 +125,7 @@
     var graphify = function(node, path) {
         if (!node.range || node.visited) return;
         
-        
+        // Visit nodes based on type.
         if (node.type === 'VariableDeclaration') {
             var stack = getStack(path[path.length-1], path.slice(0));
             _.each(node.declarations, function(declarator) {
@@ -133,35 +137,36 @@
             stack[node.id.name] = node.body;
             return;
         } else if (node.type === 'Identifier') {
-            //if (node.parent.type === 'Property') return;
-            var reference = getReference(path[path.length-1], node.name, path.slice(0));
+            var reference = getReference(path[path.length-1], 
+                    node.name, path.slice(0));
             if (reference) { 
                 graphify(reference, path.slice(0));
             } else {
                 console.log('reference not found: ' + node.name);
                 return; // Only gets here if a reference wasn't found.
             }
-
         } else if (node.type === 'ObjectExpression') {
             var stack = node.stack;
             for(var key in node) {
                 var child = node[key];
                 if (child instanceof Array) {
                     for (var i=0, l=child.length; i<l; i++) {
-                        if (child[i] && typeof child[i] === 'object' && child[i].type) {
-                            var keyName = child[i].key.name ? 
-                                child[i].key.name : child[i].key.value;
-                            stack[child[i].key.name || child[i].key.value] = child[i].value;
+                        if (child[i] && typeof child[i] === 'object' && 
+                                child[i].type) {
+                            stack[child[i].key.name || child[i].key.value] = 
+                                child[i].value;
                         }
                     }
                 }
             }
         } else if (node.type === 'MemberExpression') {
-            var reference = getReference(path[path.length-1], node.object.name, path.slice(0));
+            var reference = getReference(path[path.length-1], 
+                    node.object.name, path.slice(0));
             if (reference) {
                 // populate stack for object if the stack is empty.
                 if (!reference.stack[node.property.name]) graphify(reference);
-                graphify(reference.stack[node.property.name || node.property.value], path.slice(0));
+                graphify(reference.stack[node.property.name || 
+                        node.property.value], path.slice(0));
             } else {
                 console.log('reference not found: ' + node.name);
                 return; // Only gets here if a reference wasn't found.
@@ -169,17 +174,22 @@
 
         } else {
             node.visited = true;
+
+            // flags referenced function declarations as visited.
             if (node.type === 'BlockStatement' && node.parent &&
                     node.parent.type === 'FunctionDeclaration') {
                 node.parent.visited = true;
             }
+
+            // TODO: abstract this walking for the generic case.
             for(var key in node) {
                 if (key === 'parent') return;
                 
                 var child = node[key];
                 if (child instanceof Array) {
                     for (var i=0, l=child.length; i<l; i++) {
-                        if (child[i] && typeof child[i] === 'object' && child[i].type) {
+                        if (child[i] && typeof child[i] === 'object' && 
+                                child[i].type) {
                             graphify(child[i], path.concat(node));
                         }
                     }
@@ -190,20 +200,28 @@
         }
     };
    
+    // Pass to initialize helpers and stacks.
     walk(tree, undefined, function(node) {
         if (node.type && affectsScope(node.type)) {
             node.stack = node.stack || {'stackType': node.type};
         }
     });
+    
     graphify(tree, [], 0);
     
-    walk(tree, undefined, deleteWalk);
-    //console.log('\n\n\n\n\n');
+    /**
+     * Pass to delete unused functions.
+     * unvisited functions are assumed to be unused.
+     */
+    walk(tree, undefined, function(node) {
+        if (node.type && 
+            (node.type === 'FunctionExpression' || 
+             node.type === 'FunctionDeclaration') &&
+            !node.visited) {
+                removeDeclaration(node);
+        }
+    });
     
-    //console.log(tree.body[0].expression.callee);
-    
-    console.log('\n\n');
-
-    //tree.body[1].update('')
-    //console.log(result.toString().trim());
+    console.log('');
+    //console.log(result.toString().trim()); // output result source.
 })();
