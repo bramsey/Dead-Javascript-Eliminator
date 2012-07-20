e94c47b9179ff09b81e5e268abe3d15c5d82ddb2
Bill Ramsey <bill@billiamram.com>
2012-07-20 17:47:32 -0400

    added test cases for assignment expression functino expressions and variable declarator call expressions

---
+++ test/cases/assignment-expression.function-expression.js
@@ -0,0 +1,11 @@
+var one, two;
+
+one = function() {
+    return 1;
+};
+
+two = function() {
+    return 2;
+};
+
+two();
---
+++ test/cases/variable-declarator.call-expression.js
@@ -0,0 +1,11 @@
+var one = function() {
+    return 1;
+};
+
+var two = function() {
+    return 2;
+};
+
+var three = one();
+
+two();
